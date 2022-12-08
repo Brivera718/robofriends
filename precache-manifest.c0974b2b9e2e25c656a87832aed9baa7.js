6f841131cdb8fa7e58499ef8be8e88778efb4289
Brivera718 <110059683+Brivera718@users.noreply.github.com>
2022-12-08 18:43:21 -0500

    Updates

---
+++ precache-manifest.c0974b2b9e2e25c656a87832aed9baa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaf9cfac9bb9231a7e71",
+    "url": "/Robofriends/static/css/main.a2742287.chunk.css"
+  },
+  {
+    "revision": "eaf9cfac9bb9231a7e71",
+    "url": "/Robofriends/static/js/main.a50777ad.chunk.js"
+  },
+  {
+    "revision": "943a11e6ae08b6e165ab",
+    "url": "/Robofriends/static/js/runtime~main.b6033968.js"
+  },
+  {
+    "revision": "a9cd58522294b15bad49",
+    "url": "/Robofriends/static/css/2.8f48c633.chunk.css"
+  },
+  {
+    "revision": "a9cd58522294b15bad49",
+    "url": "/Robofriends/static/js/2.ef93a9b1.chunk.js"
+  },
+  {
+    "revision": "8fa34ba00032514f3d70",
+    "url": "/Robofriends/static/js/3.490731cf.chunk.js"
+  },
+  {
+    "revision": "0b7a37b77a8cd31f2859370e883881bb",
+    "url": "/Robofriends/index.html"
+  }
+];
